ec561f7a42c8aa4091958016a184def396cc6a30
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2017-03-02 00:55:25 +0000

    initial commit :rocket:

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('title', 'Homepage ye!')
+
+@section('content')
+    <header class="headline">
+        <h1 class="headline-title">Homepage</h1>
+        <p class="headline-text">This is a really cool application demonstrating some core features of Laravel.</p>
+    </header>
+@endsection
